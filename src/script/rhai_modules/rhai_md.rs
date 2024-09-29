b7ceaec0a8bd75585220f6a22e503fc69f121f61
Stephen Funk <stephenlfunk@gmail.com>
2024-09-28 21:02:45 -0400

    Added docscript to the modules and functions in the "src/script/rhai_modules/*" files.

--- src/script/rhai_modules/rhai_md.rs
+++ src/script/rhai_modules/rhai_md.rs
@@ -1,3 +1,13 @@
+//! Defines the `md` module, used in the rhai engine. 
+//! 
+//! ## RHAI Documentation
+//! The `md` module exposes functions that process markdown content. Useful for
+//! processing LLM responses.
+//! 
+//! ### Functions
+//! * `extract_blocks(md_content: string) -> Vec<MdBlock>`
+//! * `extract_blocks(md_content: string, lang_name: string) -> Vec<MdBlock>`
+
 use crate::support::md;
 use crate::types::MdBlock;
 use rhai::plugin::RhaiResult;
@@ -20,12 +30,28 @@ pub fn rhai_module() -> Module {
 
 // region:    --- Rhai Functions
 
+/// ## RHAI Documentation
+/// ```rhai
+/// extract_blocks(md_content: string) -> Vec<MdBlock>
+/// ```
+/// 
+/// Parses the markdown provided by `md_content` and extracts each code block, 
+/// returning a list of blocks.
 fn extract_blocks(md_content: &str) -> RhaiResult {
 	let blocks = md::extract_blocks(md_content, None);
 	let blocks: Vec<Dynamic> = blocks.into_iter().map(MdBlock::into_dynamic).collect();
 	Ok(blocks.into())
 }
 
+/// ## RHAI Documentation
+/// ```rhai
+/// extract_blocks(md_content: &str, lang_name: &str) -> Vec<MdBlock>
+/// ```
+/// 
+/// Parses the markdown provided by `md_content` and extracts each code block,
+/// returning only the blocks with a 
+/// [language identifier](https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/creating-and-highlighting-code-blocks#syntax-highlighting)
+/// that matches `lang_name`.
 fn extract_blocks_with_name(md_content: &str, lang_name: &str) -> RhaiResult {
 	let blocks = md::extract_blocks(md_content, Some(lang_name));
 	let blocks: Vec<Dynamic> = blocks.into_iter().map(MdBlock::into_dynamic).collect();
