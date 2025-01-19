b334101958f7aec8f22a4f82cd7af6e655628cf0
Jeremy Chone <jeremy.chone@gmail.com>
2025-01-19 10:11:25 -0800

    + agent - add craft module agent

---
+++ _init/agents/craft/lua/craft.lua
@@ -0,0 +1,76 @@
+
+
+-- Returns FileRecord
+function prep_input_file(input, options) 
+  options = options or {}
+  local default_name = options.default_name or "_craft"
+  local placeholder_suffix = options.placeholder_suffix or "Write your content"
+
+  -- Enter default file_stem
+  if input == nil then
+    input = default_name
+  end
+
+  -- Determine the path
+  local path = nil
+  if type(input) == "string" then
+      path = utils.text.ensure(input, {prefix = "./", suffix = ".md"})
+  else
+      path = input.path
+  end
+
+  -- create if needed
+  local first_time = utils.path.exists(path) ~= true
+  local placeholder_content = "placeholder - " .. placeholder_suffix
+  placeholder_content = placeholder_content .. " \n\n====\n\n"
+  utils.file.ensure_exists(path,placeholder_content)  
+
+  -- open if first time
+  if first_time then 
+    utils.cmd.exec("code", {path})
+  end
+
+  return utils.file.load(path)
+end
+
+-- Will return a string if this task should be skipped
+-- Returns the message to display to the user
+function should_skip(content) 
+  local str = utils.text.trim(content) -- Trim leading and trailing spaces
+
+  -- If there is no non-whitespace, then it is empty, so skip.
+  -- if not content:find("%S") then
+  if str == "" then
+      return devai.skip("Empty file - skipping for now. Start writing, and do a Redo.")
+  end
+
+  -- if starts with placeholder
+  if str:sub(1, 11):lower() == "placeholder" then
+      return devai.skip("Content is a placeholder, so skipping for now")
+  end 
+
+  return nil
+end
+
+-- retuns `inst, content` and each can be nil if 
+function prep_inst_and_content(content, separator, is_content_default) 
+  local first_part, second_part = utils.text.split_first(content, separator)
+
+  local inst, content = nil, nil
+  if second_part ~= nil then 
+    inst = first_part
+    content = second_part
+  elseif is_content_default then
+    content = first_part
+  else 
+    inst = first_part
+  end
+    
+  return inst, content
+end
+
+return {
+  prep_input_file = prep_input_file,
+  should_skip = should_skip,
+  prep_inst_and_content = prep_inst_and_content
+}
